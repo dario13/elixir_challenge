a131262863c42f5ddb892295d74b75a62bad8b0d
dario <dariogabalec@gmail.com>
2023-06-08 19:50:21 -0300

    feature: solution

--- server.py
+++ server.py
@@ -5,15 +5,70 @@ import pymongo
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/signup', methods=['POST'])
-def signup():
-    client = pymongo.MongoClient("mongodb+srv://interview:interview@cluster0.14k3kof.mongodb.net/?retryWrites=true&w=majority")
-    db = client['elixir-interview-july-2022']
-    all_users = [user for user in db.users.find()]
+client = pymongo.MongoClient(
+    "mongodb+srv://interview:interview@cluster0.14k3kof.mongodb.net/?retryWrites=true&w=majority"
+)
 
-    return {'success': True, 'request': request.form, 'users': len(all_users)}, 200
+db = client["elixir-interview-july-2022"]
+
+
+def find_username(username: str, db) -> bool:
+    try:
+        user = db.users.find_one({"username": username})
+
+        if user is not None:
+            return True
+
+        return False
+    except:
+        return False
+
+
+def find_email(email: str, db) -> bool:
+    try:
+        email = db.users.find_one({"email": email})
+
+        if email is not None:
+            return True
+
+        return False
+    except:
+        return False
+
+
+def create_user(username: str, email: str, password: str, db) -> bool:
+    try:
+        db.users.insert_one(
+            {"username": username, "email": email, "password": password}
+        )
+        return True
+    except:
+        return False
+
+
+@app.route("/api/signup", methods=["POST"])
+def signup():
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if find_username(username, db):
+        return {"message": "The username already exists"}, 400
+
+    if find_email(email, db):
+        return {"message": "The email already exists"}, 400
+
+    if create_user(username, email, password, db):
+        return {"message": "User created"}, 200
+    else:
+        return {"message": "There was a problem creating the user"}, 500
+
 
 @app.after_request
 def add_header(response):
-    response.headers['Cache-Control'] = 'no-store'
+    response.headers["Cache-Control"] = "no-store"
     return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const validateEmail = (email) => {
+    // Validate email format
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let errors = {};
+
+    if (username.length < 5)
+      errors.username = "Username must be at least 5 characters long";
+    if (!validateEmail(email)) errors.email = "Email is not valid";
+    if (password.length < 6)
+      errors.password = "Password must be at least 6 characters long";
+
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+    } else {
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:5000/api/signup",
+          new URLSearchParams({ username, email, password })
+        );
+        if (response.status === 200) {
+          // if the response is successful, redirect to the logged page
+          window.location.href = "/loggedPage";
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 400) {
+          // if there are server-side validation errors
+          setErrors(error.response.data);
+        }
+      }
+    }
+  };
+
   return (
     <div className="container">
+      {/* head */}
       <head>
         <title>Sign up for Vaepr | Vaepr.io</title>
         <link rel="icon" href="/favicon.ico" />
@@ -19,28 +71,49 @@ function App() {
           launches.
         </p>
 
-        <form action="http://127.0.0.1:5000/api/signup" method="POST">
+        <form onSubmit={handleSubmit}>
           <div className="grid">
             <div className="card">
               <div className="label">
-                <label htmlFor="username">Userame</label>
+                <label htmlFor="username">Username</label>
+                <div>{errors.username}</div>
               </div>
               <div>
-                <input id="username" name="username" type="text" />
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
               </div>
 
               <div className="label">
                 <label htmlFor="email">Email</label>
+                <div>{errors.email}</div>
               </div>
               <div>
-                <input id="email" name="email" type="text" />
+                <input
+                  id="email"
+                  name="email"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
 
               <div className="label">
-                <label htmlFor="text">Password</label>
+                <label htmlFor="password">Password</label>
+                <div>{errors.password}</div>
               </div>
               <div>
-                <input id="password" name="password" type="text" />
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
               <br />
               <button type="submit">Sign up!</button>
@@ -50,8 +123,7 @@ function App() {
       </main>
 
       <footer className="footer">
-        Powered by{' '}
-        <span>Vaepr | ware</span>
+        Powered by <span>Vaepr | ware</span>
       </footer>
     </div>
   );
